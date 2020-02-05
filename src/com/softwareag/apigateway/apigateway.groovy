ca19edc68d420e2f1a188eb19b533dac7a789a9d
sree <sreesaran.developer@gmail.com>
2020-02-05 17:58:46 +0530

    added enhancements in shutdown and restartcommands

--- src/com/softwareag/apigateway/apigateway.groovy
+++ src/com/softwareag/apigateway/apigateway.groovy
@@ -1,26 +1,67 @@
-#!/usr/bin/groovy
 
 package src.com.softwareag.apigateway
 
-def shutdown(installDir, tenant){
-    //check based on platform.
-    installationDir = "${installDir}/profiles/IS_" + "${tenant}" + "/bin"
-    dir(installationDir){
-        bat "shutdown.bat"
+    def shutdown(installDir, tenant){
+
+        if (System.properties['os.name'].toLowerCase().contains('windows')) {
+        installationDir = "${installDir}/profiles/IS_" + "${tenant}" + "/bin"
+        dir(installationDir){
+            bat "shutdown.bat"
+        }
+        }
+        else {
+            installationDir = "${installDir}\\profiles\\IS_" + "${tenant}" + "\\bin"
+            dir(installationDir){
+                sh "shutdown.sh"
+            }
+        }
     }
 
-    //Make sure LOCKFILE is deleted.
-    //delete elasticsearch.pid also
+
+    def startup(installDir, tenant){
+
+        if (System.properties['os.name'].toLowerCase().contains('windows')) {
+            installationDir = "${installDir}/profiles/IS_" + "${tenant}" + "/bin"
+            dir(installationDir) {
+                bat "startup.bat"
+            }
+        }
+        else {
+            installationDir = "${installDir}\\profiles\\IS_" + "${tenant}" + "\\bin"
+            dir(installationDir){
+                sh "startup.sh"
+            }
+        }
+   }
+
+
+def verifyAPIGatewayIsRunning() {
+    print("Try to check APIGateway is up and running")
+    retryAttempts = 10
+    attempt = 0
+    while(attempt<retryAttempts) {
+        if(pingService()==200) {
+            print("API Gateway is up and running")
+            return  true
+        }
+        Thread.sleep("10000")
+        attempt++
+        print("sleeping for 10 seconds");
+        print("Total sleep time "+attempt*10)
+
+    }
+    print("APIGateway is not getting started")
+    return false
 }
 
-def startup(installDir, tenant){
-    //check based on platform.
-    installationDir = "${installDir}/profiles/IS_" + "${tenant}" + "/bin"
-    dir(installationDir){
-        bat "startup.bat"
-    }
+def pingService() {
+    //assuming the scripts run in the same environment as apigateway
+    def get = new URL("http://localhost:5555/rest/apigateway/health").openConnection();
+    get.setRequestProperty("Content-Type", "application/json");
+    get.setRequestProperty("Accept", "application/json")
+    return get.getResponseCode();
+}
 
-    //Make sure LOCKFILE is deleted.
-    //delete elasticsearch.pid also
-
+static  void main(String[] args) {
+    verifyAPIGatewayIsRunning()
 }
