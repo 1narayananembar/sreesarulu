ce5ecc231de65e6fc131349f3dc3e39e7acfb779
sree <sreesaran.developer@gmail.com>
2020-02-06 19:32:04 +0530

    added methods for Installation uninstallation silentscript replacement

--- src/com/softwareag/apigateway/apigateway.groovy
+++ src/com/softwareag/apigateway/apigateway.groovy
@@ -1,16 +1,20 @@
 
 package src.com.softwareag.apigateway
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
     def shutdown(installDir, tenant){
         installationDir = "${installDir}/profiles/IS_" + "${tenant}" + "/bin"
+        dir(installationDir) {
+            if (System.properties['os.name'].toLowerCase().contains('windows')) {
 
-        if (System.properties['os.name'].toLowerCase().contains('windows')) {
-        dir(installationDir){
-            bat "shutdown.bat"
-        }
-        }
-        else {
-            dir(installationDir){
+                bat "shutdown.bat"
+
+            } else {
                 sh "shutdown.sh"
             }
         }
@@ -19,13 +23,12 @@ package src.com.softwareag.apigateway
 
     def startup(installDir, tenant){
         installationDir = "${installDir}/profiles/IS_" + "${tenant}" + "/bin"
-        if (System.properties['os.name'].toLowerCase().contains('windows')) {
-            dir(installationDir) {
+        dir(installationDir) {
+            if (System.properties['os.name'].toLowerCase().contains('windows')) {
+
                 bat "startup.bat"
-            }
-        }
-        else {
-            dir(installationDir){
+
+            } else {
                 sh "startup.sh"
             }
         }
@@ -62,6 +65,117 @@ def pingService() {
         return get.getResponseCode();
 }
 
+def installAPIGateway(jarLocation,installDir, readscript) {
+
+    dir(jarLocation) {
+        if (System.properties['os.name'].toLowerCase().contains('windows')) {
+            bat "java -jar SoftwareAGInstaller.jar -installDir " + $ { installDir } + " -readScript " + "${readscript}"
+        } else {
+            sh "java - jar SoftwareAGInstaller.jar -installDir " + $ { installDir } + " -readScript " + "${readscript}"
+        }
+    }
+}
+
+def unInstallAPIGateway(installationDir){
+
+
+    dir(installationDir) {
+        if (System.properties['os.name'].toLowerCase().contains('windows')) {
+            bat "java -jar ${installDir}/install/Distman.jar -installDir " + $ {installDir} + " -readUninstallScript -console"
+        }
+        else {
+            sh "java -jar ${installDir}/install/Distman.jar -installDir " + $ {installDir} + " -readUninstallScript -console"
+        }
+    }
+}
+
+//download jar
+def downloadInstallationJar(String fileURL, String saveDir)
+{
+    try {
+        int BUFFER_SIZE = 4096;
+        URL url = new URL(fileURL);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        int responseCode = httpConn.getResponseCode();
+
+        // always check HTTP response code first
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            String fileName = "";
+            String disposition = httpConn.getHeaderField("Content-Disposition");
+            String contentType = httpConn.getContentType();
+            int contentLength = httpConn.getContentLength();
+
+            if (disposition != null) {
+                // extracts file name from header field
+                int index = disposition.indexOf("filename=");
+                if (index > 0) {
+                    fileName = disposition.substring(index + 10,
+                            disposition.length() - 1);
+                }
+            } else {
+                // extracts file name from URL
+                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
+                        fileURL.length());
+            }
+
+            System.out.println("Content-Type = " + contentType);
+            System.out.println("Content-Disposition = " + disposition);
+            System.out.println("Content-Length = " + contentLength);
+            System.out.println("fileName = " + fileName);
+
+            // opens input stream from the HTTP connection
+            InputStream inputStream = httpConn.getInputStream();
+            String saveFilePath = saveDir + File.separator + fileName;
+
+            // opens an output stream to save into file
+            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+            int bytesRead = -1;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            System.out.println("File downloaded");
+        } else {
+            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+            error("No file to download. Server replied HTTP code: " + responseCode);
+        }
+        httpConn.disconnect();
+    }
+    catch (Exception e)
+    {
+        print("Error in downloading jar file "+e)
+        error("Issue in downloading the Installer jar " + e)
+    }
+}
+
+
+
+//ping all native services
+def pingAllNativeServices() {
+
+}
+
+//replace silent script parameters
+def replaceSilentScript(location,params) {
+    //Assuming the name of the script file doesnt change
+    def originalScript = new File(location+'silentScript.txt')
+    for(parameter in params.keySet()) {
+        def temp  = originalScript.text.replace(parameter,params.get(parameter))
+        originalScript.text = temp
+    }
+}
+
+
+
 static  void main(String[] args) {
-    verifyAPIGatewayIsRunning()
+    //can we use the same installer jar for all version ?
+    //downloadInstallationJar("http://aquarius_dae.eur.ad.sag/PDShare/WWW/dataserve107oct2020_SIC/data/SoftwareAGInstaller1.jar","C:/Users/srag/Downloads")
+    //def params  = ['${InstallationLocation}':'','${Serverurl}':'','${microgatewaylicense}':'','${apigatewaylicense}':'','${licenselocation}':'']
+    //replaceSilentScript(params)
+
 }
